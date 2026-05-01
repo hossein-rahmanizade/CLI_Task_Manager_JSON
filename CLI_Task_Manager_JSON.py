56bee30be9eac7a314da49de81ac75158eabe26d
hossein <hossein.rahmanizadeh1378@gmail.com>
2026-05-01 21:36:46 +0330

    Add timestamp and delete task function and some minor fixes

--- CLI_Task_Manager_JSON.py
+++ CLI_Task_Manager_JSON.py
@@ -1,7 +1,10 @@
+import argparse
 from datetime import datetime
 import json
 import os
 import re
+import sys
+
 
 user_input= input("Please enter your task, task's priority, task's deadline seperate them with ',': ")
 
@@ -13,27 +16,55 @@ def take_task(user_task):
     user_task_keys = ["name", "priority", "deadline"]
     result = dict(zip(user_task_keys, user_task_split))
     result["date_added"] = date_added
+    result["done"] = False
     return result
 
-print(f"User's task information is {take_task(user_input)}.")
 
-if not os.path.exists('/tmp/tasks.json'):
-    with open('/tmp/tasks.json', 'w') as file:
-        data = take_task(user_input)
-        data["id"] = 1
-        json.dump([data], file, indent=4, sort_keys=True)
+json_path = "/tmp/tasks.json"
 
-else:
-    with open('/tmp/tasks.json', 'r') as file:
-        data = json.load(file)
-        next_id = max((item['id'] for item in data), default=0) + 1
+def add_task():
+    if not os.path.exists(json_path):
+        with open(json_path, 'w') as file:
+            data = take_task(user_input)
+            data["id"] = 1
+            json.dump([data], file, indent=4, sort_keys=True)
 
-        new_entry = take_task(user_input)
-        new_entry["id"] = next_id
+    else:
+        with open(json_path, 'r') as file:
+            data = json.load(file)
+            next_id = max((item['id'] for item in data), default=0) + 1
 
-        data.append(new_entry)
-        
-    with open('/tmp/tasks.json', 'w') as file:
-        json.dump(data, file, indent=4, sort_keys=True)
+            new_entry = take_task(user_input)
+            new_entry["id"] = next_id
 
-        
+            data.append(new_entry)
+            
+        with open(json_path, 'w') as file:
+            json.dump(data, file, indent=4, sort_keys=True)
+    
+    print("Your task been added.")
+
+add_task()
+
+
+def delete_task(chosen_name):
+    if os.path.exists(json_path):
+        with open(json_path, 'r') as file:
+            data = json.load(file)
+            for i in range(len(data)-1, -1, -1):
+                if data[i]["name"] == chosen_name:
+                    data.pop(i)
+
+        with open(json_path, 'w') as file:
+            json.dump(data, file, indent=4, sort_keys=True)        
+        return f"You're task {chosen_name} been deleted"    
+    return "There's no task here to delete."
+
+
+def mark_down_task():
+    pass
+
+
+def interactive_mode():
+    pass
+
--- README.md
+++ README.md
@@ -6,20 +6,20 @@
   - [x] Add task to a file
   - [ ] List tasks
   - [ ] Mark done
-  - [ ] Delete task
+  - [x] Delete task
   - [x] Store in `tasks.json`
   - [ ] Each task:
         {
           "id": 1,
           "name": "...",
-          "status: "done",
+          "done: False,
           "priority": 1-5
         }
 
 ## Code cleanup:
 
-- [ ] Need to add timestamp to user's input
+- [x] Need to add timestamp to user's input
 - [ ] Handle errors in the take_task() for edge case inputs
 - [ ] Add main and while to keep getting user's input unless user stops it
-- [ ] Add args for faster task taking from terminal, add flags like {"-a":"add task","-d":"delete","-v":"view tasks",...}
+- [ ] Add args for faster task taking from terminal, add flags like {"-a":"add task","-d":"delete","-v":"view tasks",...}, so to handle this, I need to have 2 general state for my program, one is like main-command-line mode and the other one interactive mode 
 - [x] Handle the "id" in json file
